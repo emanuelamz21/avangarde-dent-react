15008af0ef10f5a0ca7eb7546f4b16ef62835eb9
emanuelamz21 <emanuel.amuza21@gmail.com>
2023-10-21 11:32:13 -0500

    Contact Page Finished

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -1,48 +1,32 @@
 import styles from "./ContactComponent.module.css";
 
 function ContactComponent() {
-  const locations = [
-    {
-      name: "Avangarde Dent",
-      address: "Bulevardul Ion Mihălache 140, București",
-      hours: [
-        "Luni - Vineri: 10:00 - 18:00",
-        "Sâmbătă: 10:00 - 14:00",
-        "Duminică: Închis",
-      ],
-      phone: "+4072861928",
-      email: "avangarde.dent@gmail.com",
-      map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.3741490112798!2d26.063412576809178!3d44.466502999450064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b203a85b679c2d%3A0x3e2ce83623a83d5e!2sAvangarde%20Dent!5e0!3m2!1sen!2sro!4v1697646501808!5m2!1sen!2sro",
-    },
-  ];
-
   return (
-    <div className={styles.background}>
-      {locations.map((location) => (
-        <div key={location.name} className={styles.contactInformation}>
-          <iframe
-            src={location.map}
-            width="639"
-            height="406"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+    <div className={styles.container}>
+      <div className={styles.contactInformation}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2847.3741490112798!2d26.063412576809178!3d44.466502999450064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b203a85b679c2d%3A0x3e2ce83623a83d5e!2sAvangarde%20Dent!5e0!3m2!1sen!2sro!4v1697646501808!5m2!1sen!2sro"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
 
-          <div className={styles.contactDetails}>
-            <h3>{location.name}</h3>
-            <p>{location.address}</p>
-            <h3>Program</h3>
-            {location.hours.map((hour) => (
-              <p key={hour}>{hour}</p>
-            ))}
-            <h3>Contact</h3>
-            <p>Telefon: {location.phone}</p>
-            <p>Email: {location.email}</p>
-          </div>
+        <div className={styles.contactDetails}>
+          <h3>Avangarde Dent</h3>
+          <p>Bulevardul Ion Mihălache 140, București</p>
+          <br></br>
+          <h3>Program</h3>
+          <p>Luni - Vineri: 10:00 - 18:00</p>
+          <p>Sâmbătă: 10:00 - 14:00</p>
+          <p>Duminică: Închis</p>
+          <br></br>
+          <h3>Contact</h3>
+          <p>Telefon: +4072861928</p>
+          <p>Email: avangarde.dent@gmail.com</p>
+          <br></br>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,18 +1,20 @@
 import ContactComponent from "../components/ContactComponent";
 import PageNav from "../components/PageNav";
 import SectionInformation from "../components/SectionInformation";
+import Footer from "../components/Footer";
+import styles from "./Contact.module.css";
 
 const info = {
   header: "Contact - Avangarde Dent",
   detailsHeader: "Contact Cabinet",
   detailsParagraph:
     "Suntem aici pentru dumneavoastră. Contactați-ne acum și descoperiți unde suntem!",
-  padding: "pt-12",
+  padding: "pt-12 ",
 };
 
 function Contact() {
   return (
-    <>
+    <div className={styles.background}>
       <PageNav />
       <SectionInformation
         header={info.header}
@@ -21,7 +23,8 @@ function Contact() {
         padding={info.padding}
       />
       <ContactComponent />
-    </>
+      <Footer />
+    </div>
   );
 }
 
