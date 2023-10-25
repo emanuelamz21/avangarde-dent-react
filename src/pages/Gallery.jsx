0354326a06610836702adf85a4f65c479e833f3f
emanuelamz21 <emanuel.amuza21@gmail.com>
2023-10-25 15:31:26 -0500

    Transfer For Laptop

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,26 @@
 import PageNav from "../components/PageNav";
+import styles from "./Gallery.module.css";
 
 function Gallery() {
   return (
     <>
       <PageNav />
-      <div>Galerry</div>
+      <div className={styles.container}>
+        <img
+          src="https://picsum.photos/800/600"
+          alt="random"
+          className={styles.mainPhoto}
+        />
+        <img src="https://picsum.photos/300/250" alt="random" />
+        <img src="https://picsum.photos/300/250" alt="random" />
+        <div className={styles.bottomRow}>
+          <img src="https://picsum.photos/200/200" alt="random" />
+          <img src="https://picsum.photos/200/200" alt="random" />
+          <img src="https://picsum.photos/200/200" alt="random" />
+        </div>
+      </div>
     </>
   );
 }
-
+// 600.600
 export default Gallery;
