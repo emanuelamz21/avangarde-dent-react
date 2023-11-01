d2855876c59dc4d325f1cd390d775113574ee1e6
emanuelamz21 <emanuel.amuza21@gmail.com>
2023-11-01 05:17:14 -0500

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 function Footer() {
@@ -6,23 +7,46 @@ function Footer() {
       <div className={styles.footerContainer}>
         <h3>Tratamente</h3>
         <ul>
-          <li>Grija Dentara</li>
-          <li>Implant Dentar</li>
-          <li>Stomatologie Generala</li>
-          <li>Extractii Dentare</li>
-          <li>Stomatologie Restaurative</li>
-          <li>Parodontologie</li>
+          <NavLink to="/treatments">
+            <li>Paradontologie</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Obturații fizionomice</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Coroane, Fațete dentare și Punți</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Implantologie</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Chirurgie dentoalveolară</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Profilaxie dentară si Albiri Dentare</li>
+          </NavLink>
         </ul>
         <p>© Avangarde Dent 2023. Toate drepturile rezervate.</p>
       </div>
       <div className={styles.footerContainer}>
         <h3>Echipa Avangarde Dent</h3>
         <ul>
-          <li>Acasa</li>
-          <li>Echipa</li>
-          <li>Tratamente</li>
-          <li>Galerie</li>
-          <li>Contact</li>
+          <NavLink to="/">
+            <li>Acasa</li>
+          </NavLink>
+          <NavLink to="/heritage">
+            <li>Echipa</li>
+          </NavLink>
+          <NavLink to="/treatments">
+            <li>Tratamente</li>
+          </NavLink>
+          <NavLink to="/gallery">
+            <li>Galerie</li>
+          </NavLink>
+          <NavLink to="/contact">
+            <li>Contact</li>
+          </NavLink>
+
           <br />
         </ul>
         <img src="logo.png" alt="logo" className={styles.icon} />
